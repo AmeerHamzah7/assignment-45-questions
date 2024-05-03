863287eb88c1e250da1f321f1c884009400c7c1b
Ameer Hamza <hamzah95@live.com>
2024-05-03 23:03:32 +0500

    assignment

--- Q6/q6.js
+++ Q6/q6.js
@@ -1,5 +1,5 @@
 //Q6: Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.
-var personName = "Osman";
+var personName = "Hamza";
 console.log("\t ".concat(personName, " \n"));
 //now striping the white spaces
 var strippingSpaces = personName.trim();
