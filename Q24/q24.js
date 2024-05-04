75be24dc074074efb720ccf0ac464d75bcbb68a1
Ameer Hamza <hamzah95@live.com>
2024-05-04 17:22:57 +0500

    assignment

---
+++ Q24/q24.js
@@ -0,0 +1,16 @@
+//More Conditional Tests: You don’t have to limit the number of tests you create to 10. If you want to try more comparisons, write more tests. Have at least one True and one False result for each of the following:
+//making variables for the exercise
+var color = "black";
+var uppercased = "BLACK";
+//Tests for equality and inequality with strings
+console.log("Testing if color is equal black");
+console.log(color == "black");
+//Tests using the lower case function
+console.log("\nTesting if black is equal to the lowercased black");
+console.log(uppercased != "black");
+console.log("\nTesting if uppercased variable is not equal to the lowercasedColor");
+console.log(uppercased == uppercased.toLowerCase());
+//Numerical tests involving equality and inequality, greater than and less than, greater than or equal to, and less than or equal to
+//Tests using "and" and "or" operators
+//Test whether an item is in a array
+//Test whether an item is not in a array
