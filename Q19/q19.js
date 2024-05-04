3a7c048e0d2719ed67ed7410d9b4dcc6f13ad06c
Ameer Hamza <hamzah95@live.com>
2024-05-04 08:30:40 +0500

    assignment

---
+++ Q19/q19.js
@@ -0,0 +1,5 @@
+//Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+var guestList = ["Hamza", "Daniyal", "Usman", "Farhan", "Aamir"];
+//guestList.forEach(guest => console.log(`Hey Mr. ${guest}, you're invited to the dinner`));
+//checking number of guests invited
+console.log("We are inviting ".concat(guestList.length, " guests"));
---
+++ Q19/q19.ts
@@ -0,0 +1,7 @@
+//Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+
+let guestList:string[] = ["Hamza", "Daniyal", "Usman", "Farhan", "Aamir"];
+//guestList.forEach(guest => console.log(`Hey Mr. ${guest}, you're invited to the dinner`));
+
+//checking number of guests invited
+console.log(`We are inviting ${guestList.length} guests`);
